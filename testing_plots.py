009a6584a15ab41dd991d23c08c0d5a0193182ac
Haley Fica <haleyfica@Haleys-MacBook-Pro.local>
2015-05-28 15:50:53 -0400

    added the beautiful graph yo

--- testing_plots.py
+++ testing_plots.py
@@ -1,3 +1,5 @@
+'''the graph stuff'''
+
 import numpy as np
 import matplotlib as mpl
 x_range = np.arange(0.1, 10., 0.01)
@@ -5,3 +7,4 @@ np.sin(np.pi/2)
 import matplotlib.pyplot as plt
 y_range = np.sin(x_range)
 plt.plot(x_range, y_range, 'g')
+plt.savefig('mysinegraph2')
