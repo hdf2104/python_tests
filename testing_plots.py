d3e3c431ea13ccc8cf80129d8e184c1a73edacd7
Haley Fica <haleyfica@Haleys-MacBook-Pro.local>
2015-05-28 15:39:10 -0400

    added the testing stuff

---
+++ testing_plots.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib as mpl
+x_range = np.arange(0.1, 10., 0.01)
+np.sin(np.pi/2)
+import matplotlib.pyplot as plt
+y_range = np.sin(x_range)
+plt.plot(x_range, y_range, 'g')
